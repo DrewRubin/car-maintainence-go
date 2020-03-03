c67c69acc21ae62485574ed11f2ef3595413f896
Drew Rubin <drewrubin@outlook.com>
2020-03-02 19:21:14 -0500

    records working

--- main.go
+++ main.go
@@ -37,11 +37,13 @@ func main() {
 	} else if argsWithoutProg[0] == "--remove-vehicle" {
 		removeVehicle()
 	} else if argsWithoutProg[0] == "--add-record" {
-
+		addRecord()
 	} else if argsWithoutProg[0] == "--remove-record" {
 
 	} else if argsWithoutProg[0] == "--view-vehicles" {
 		viewVehicles()
+	} else if argsWithoutProg[0] == "--view-record" {
+		viewRecord()
 	} else {
 
 	}
@@ -79,10 +81,21 @@ func addVehicle() {
 }
 
 func removeVehicle() {
-	// viewVehicles()
-	// reader := bufio.NewReader(os.Stdin)
-	// fmt.Println("Which vehicle number do you want to remove?")
-	// vehicleNumber, _ := reader.ReadString('\n')
+	viewVehicles()
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Which vehicle number do you want to remove?")
+	vehicleNumber, _ := reader.ReadString('\n')
+
+	if _, err := os.Stat("./vehicles.db"); os.IsNotExist(err) {
+		os.Create("./vehicles.db")
+	}
+	database, _ := sql.Open("sqlite3", "./vehicles.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS vehicles (id INTEGER PRIMARY KEY, make TEXT, model TEXT, year INT, mileage INT, tag TEXT)")
+	statement.Exec()
+	// statement, _ = database.Prepare("INSERT INTO vehicles (make, model, year, mileage, tag) VALUES (?,?,?,?,?)")
+	statement, _ = database.Prepare("DELETE FROM vehicles WHERE id=?")
+	statement.Exec(vehicleNumber)
+	database.Close()
 
 }
 func viewVehicles() {
@@ -110,4 +123,74 @@ func viewVehicles() {
 	}
 	database.Close()
 
+}
+func viewRecord() {
+	//id INTEGER PRIMARY KEY, vehicleid INTEGER, date TEXT, mileage INT, cost INT, description TEXT
+	viewVehicles()
+	fmt.Println("Which vehicle number do you want to view records for?")
+	reader := bufio.NewReader(os.Stdin)
+	vehicleNumber, _ := reader.ReadString('\n')
+	fmt.Println("id:\tDate:\tMileage:\tCost:\tDescription:")
+	if _, err := os.Stat("./vehicles.db"); os.IsNotExist(err) {
+		os.Create("./vehicles.db")
+	}
+
+	database, _ := sql.Open("sqlite3", "./vehicles.db")
+
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS vehicles (id INTEGER PRIMARY KEY, make TEXT, model TEXT, year INT, mileage INT, tag TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY, vehicleid INTEGER, date TEXT, mileage INT, cost INT, description TEXT)")
+	statement.Exec()
+	//id, date, mileage, cost, description
+	// statement, _ = database.Prepare("DELETE FROM vehicles WHERE id=?")
+	// statement.Exec(vehicleNumber)
+	// rows,err:=db.Query("select id, username from user where id = ?", 2)
+	matchingRecords, _ := database.Query("SELECT id, date, mileage, cost, description FROM records WHERE vehicleid=?", vehicleNumber)
+
+	var id int
+	var date string
+	var mileage int
+	var cost int
+	var description string
+	if matchingRecords == nil {
+		return
+	}
+	for matchingRecords.Next() {
+		matchingRecords.Scan(&id, &date, &mileage, &cost, &description)
+		fmt.Println(strconv.Itoa(id) + ") " + "\t" + date + "\t" + strconv.Itoa(mileage) + "\t$" + strconv.Itoa(cost) + "\t" + description)
+
+	}
+	database.Close()
+
+}
+func addRecord() {
+	viewVehicles()
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Which vehicle number do you want to add a record for?")
+	vehicleNumber, _ := reader.ReadString('\n')
+	fmt.Println("Record date (YYYY-MM-DD):")
+	recordDate, _ := reader.ReadString('\n')
+	recordDate = recordDate[:len(recordDate)-1]
+	fmt.Println("Mileage:")
+	mileageString, _ := reader.ReadString('\n')
+	mileageString = mileageString[:len(mileageString)-1]
+	mileage, _ := strconv.Atoi(mileageString)
+
+	fmt.Println("Cost of repair (integer):")
+	costString, _ := reader.ReadString('\n')
+	costString = costString[:len(costString)-1]
+	cost, _ := strconv.Atoi(costString)
+
+	fmt.Println("Description of service/repair:")
+	description, _ := reader.ReadString('\n')
+
+	if _, err := os.Stat("./vehicles.db"); os.IsNotExist(err) {
+		os.Create("./vehicles.db")
+	}
+	database, _ := sql.Open("sqlite3", "./vehicles.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY, vehicleid INTEGER, date TEXT, mileage INT, cost INT, description TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO records (vehicleid, date, mileage, cost, description) VALUES (?,?,?,?,?)")
+	statement.Exec(vehicleNumber, recordDate, mileage, cost, description)
+	database.Close()
 }
